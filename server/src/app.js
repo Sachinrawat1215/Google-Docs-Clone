f7e77608958dcababa877b4ddcf5765bbd709def
Sachinrawat1215 <Sachinrawat0853@gmail.com>
2024-03-17 01:50:56 +0530

    Created register user api and formatted main file

--- server/src/app.js
+++ server/src/app.js
@@ -1,35 +1,36 @@
-const { Server } = require('socket.io');
-const Connection = require('./database/db');
-const { getDocument, updateDocument } = require('./controller/document-controller');
+// Import required modules
+const express = require('express');
+const http = require('http');
 const dotenv = require('dotenv');
+const Connection = require('./database/db');
+const socketFunctions = require('./controller/socketFunctions');
+const routes = require('./routes/auth.routes');
 
+// Load environment variables
 dotenv.config();
 
-const PORT = process.env.PORT || 8000;
+// Set up Express app
+const app = express();
+const server = http.createServer(app);
 
+// Set up middleware for parsing JSON and URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ extended: true }));
+
+// Set up MongoDB connection
 const URL = process.env.MONGODB_URI;
-
 Connection(URL);
 
-const io = new Server(PORT, {
-    cors: {
-        origin: ['http://localhost:3000', 'http://localhost:3001', 'https://getdocs.vercel.app'],
-        method: ['GET', 'POST']
-    }
-});
+// Set up Socket.IO functions
+socketFunctions(server);
 
-io.on('connection', socket => {
-    socket.on('get-document', async documentId => {
-        const document = await getDocument(documentId);
-        socket.join(documentId);
-        socket.emit('load-document', document.data);
+// Set up routes
+app.use('/', routes);
 
-        socket.on('send-changes', delta => {
-            socket.broadcast.to(documentId).emit('receive-changes', delta);
-        });
+// Define port numbers
+const EXPRESS_PORT = process.env.EXPRESS_PORT || 8000;
 
-        socket.on('save-document', async data => {
-            await updateDocument(documentId, data);
-        });
-    });
+// Set up Express server
+server.listen(EXPRESS_PORT, () => {
+  console.log(`Server is running on PORT number ${EXPRESS_PORT}`);
 });
---
+++ server/src/utils/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  REQUIRED_FIELDS_MESSAGE: 'All fields are required',
+  EMAIL_EXISTS_MESSAGE: 'Email already exists',
+  REGISTRATION_SUCCESS_MESSAGE: 'User created successfully',
+  REGISTRATION_FAILED_MESSAGE: 'Registration failed',
+};
